2e2074a2d1b5c341edb758b3fc8553f31fceebc0
dilloneq77 <norbertkinass@gmail.com>
2023-11-07 13:11:45 +0100

    Add @Retention at Timeout.java

--- src/main/java/com/test/junit/anotation/Timeout.java
+++ src/main/java/com/test/junit/anotation/Timeout.java
@@ -1,5 +1,9 @@
 package com.test.junit.anotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Timeout {
     int time();
 
